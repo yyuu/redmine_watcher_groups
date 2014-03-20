beeeb60892be292ff757fb67fc50ea8e3392c7e0
Shinmura Kazuki <k_shinmura@geishatokyo.com>
2014-03-20 20:50:02 +0900

    display watcher groups even if the tracker has been changed

--- lib/watcher_groups_hook_listener.rb
+++ lib/watcher_groups_hook_listener.rb
@@ -1,8 +1,8 @@
 class WatcherGroupsHookListener < Redmine::Hook::ViewListener
   def view_issues_form_details_bottom(context={})
-    if context[:hook_caller].respond_to?(:render) and context[:controller].action_name == "new"
+    if context[:hook_caller].respond_to?(:render) and context[:request].parameters[:id].blank? and (context[:controller].action_name == "new" or context[:controller].action_name == "update_form")
         context[:hook_caller].send(:render, :partial => "watcher_groups/watcher_groups", :locals => context)
-    elsif context[:controller].is_a?(ActionController::Base) and context[:controller].action_name == "new"
+    elsif context[:controller].is_a?(ActionController::Base) and context[:request].parameters[:id].blank? and (context[:controller].action_name == "new" or context[:controller].action_name == "update_form")
         context[:controller].send(:render_to_string, :partial => "watcher_groups/watcher_groups", :locals => context)
     else
       ""
