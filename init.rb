d1cfeeb2f0c421ad5b84e4ac01fcbbbd708ce08f
Yamashita Yuu <yamashita@geishatokyo.com>
2014-03-24 19:49:27 +0900

    0.0.3git

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@ Redmine::Plugin.register :redmine_watcher_groups do
   name 'Watcher Groups plugin'
   author 'Yamashita, Yuu'
   description 'Select issue watchers by groups.'
-  version '0.0.2'
+  version '0.0.3git'
   url 'https://github.com/yyuu/redmine_watcher_groups'
   author_url 'http://www.geishatokyo.com/'
 end
