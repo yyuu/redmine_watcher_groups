fed8a957b05713d292a7b5a93d5f2b38c6cd2e0e
Eitan Har-Shoshanim <eharshosh@gmail.com>
2015-04-27 13:00:16 +0300

    + Updated to work with Redmine 2.6x (maybe even Redmine 3x - not tested) + Added Eitan H.S. as one of the authors at init.rb

--- init.rb
+++ init.rb
@@ -1,9 +1,9 @@
 require "watcher_groups_hook_listener"
 Redmine::Plugin.register :redmine_watcher_groups do
   name 'Watcher Groups plugin'
-  author 'Yamashita, Yuu'
+  author 'Yamashita, Yuu & Har-Shoshanim, Eitan'
   description 'Select issue watchers by groups.'
-  version '0.0.3git'
+  version '0.1.3git'
   url 'https://github.com/yyuu/redmine_watcher_groups'
   author_url 'http://www.geishatokyo.com/'
 end
